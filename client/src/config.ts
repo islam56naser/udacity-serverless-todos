f4b0dddcf0b7f91d98b4f3660143400ed6c9dfc1
islam56naser <islam56naser@gmail.com>
2020-04-20 12:38:47 +0200

    initial

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'ltzt3yw6k0'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-u9bgyffh.auth0.com',
+  clientId: 'TYdKcxwo96OS0sq05nRxsy9fvhYA07Yp',
+  callbackUrl: 'http://localhost:3000/callback'
+}
